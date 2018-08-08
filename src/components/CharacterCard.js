4767096110fc389b0707d77797f927bb56ae9000
Esther Pato <estherpato@gmail.com>
2018-08-08 15:15:28 +0200

    Initial commit components created

---
+++ src/components/CharacterCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CharacterCard extends Component {
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
+
+export default CharacterCard;
---
+++ src/components/CharacterList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import CharacterCard from './CharacterCard';
+
+class CharacterList extends Component {
+    render() {
+        return(
+            <div></div>
+        );
+    }
+}
+
+export default CharacterList;
